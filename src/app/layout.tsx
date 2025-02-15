aef4e5122ea8bd089e5a040728aef3fc05474ca8
Ryan D <ryandodd0115@gmail.com>
2025-02-14 23:41:25 -0600

    Layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,17 @@ import "./globals.css";
 import { dmSerif, geistSans, inter } from "@/app/ui/fonts";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '../../supabase'
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../supabase";
 
 import SessionProvider from "./components/SessionProvider";
 import Sidebar from "@/app/components/sidebar";
 import Topbar from "@/app/components/Topbar";
 
-const supabaseUrl: string = 'https://ertqiknveclsdywsbiuu.supabase.co'
-const supabaseKey: string = process.env.SUPABASE_KEY === undefined ? "" : process.env.SUPABASE_KEY
-const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+const supabaseUrl: string = "https://ertqiknveclsdywsbiuu.supabase.co";
+const supabaseKey: string =
+	process.env.SUPABASE_KEY === undefined ? "" : process.env.SUPABASE_KEY;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export const metadata: Metadata = {
 	title: "ResLife",
@@ -27,15 +28,21 @@ export default async function RootLayout({
 }>) {
 	const session = await getServerSession();
 
-	{
-		/* Redirect to login if no session */
-	}
 	if (!session || !session.user) {
 		redirect("/api/auth/signin");
 	}
 
-	console.log(session);
+	const { data, error } = await supabase
+		.from('Users')
+		.select('email')
+		.eq('email', session.user.email == null ? "" : session.user.email);
 
+	if (data?.length === 0) {
+		await supabase.from("Users").insert([{ email: session.user.email || "", name: session.user.name || "" }]).select();
+	}
+
+	console.log(data);
+	console.log(error);
 	return (
 		<html lang="en">
 			<SessionProvider session={session}>
