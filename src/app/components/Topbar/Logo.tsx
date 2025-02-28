729e8a3e68cd4ad897aafc9ddd557d0e4cd2c088
Duncan Carr <dacarr@mail.bradley.edu>
2025-02-27 23:17:11 -0600

    Add links to the dynamic paths

--- src/app/components/Topbar/Logo.tsx
+++ src/app/components/Topbar/Logo.tsx
@@ -1,31 +1,46 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-function formatPathSegments(path: string): string[] {
+interface PathSegment {
+	display: string;
+	link: string;
+}
+
+function formatPathSegments(path: string): PathSegment[] {
 	if (!path) {
 		return [];
 	}
 
 	const pathWithoutQuery = path.split("?")[0];
-
 	const segments = pathWithoutQuery.replace(/^\/|\/$/g, "").split("/");
 
-	const formattedSegments = segments.map((segment) => {
-		const words = segment.replace(/-/g, " ").split(" ");
+	if (segments.length === 1 && segments[0] === "") {
+		return [{ display: "Dashboard", link: "/" }];
+	}
 
+	const formattedSegments: PathSegment[] = [];
+	let currentPath = "";
+
+	segments.forEach((segment) => {
+		if (segment === "") return;
+
+		currentPath += `/${segment}`;
+
+		const words = segment.replace(/-/g, " ").split(" ");
 		const capitalizedWords = words.map(
 			(word) => word.charAt(0).toUpperCase() + word.slice(1),
 		);
+		const display = capitalizedWords.join(" ");
 
-		return capitalizedWords.join(" ");
+		formattedSegments.push({
+			display,
+			link: currentPath,
+		});
 	});
 
-	if (formattedSegments.length === 1 && formattedSegments[0] === "") {
-		return ["Dashboard"];
-	}
-
-	return formattedSegments.length < 1 ? [] : formattedSegments;
+	return formattedSegments;
 }
 
 export default function Logo() {
@@ -35,15 +50,20 @@ export default function Logo() {
 
 	return (
 		<div>
-			<span className="text-white/65">Example University</span>
+			<Link href="/" className="text-white/65">
+				Example University
+			</Link>
 			{" / "}
 			{pathSegments.map((segment, index) =>
 				index < pathSegments.length - 1 ? (
-					<span key={index}>{segment} / </span>
-				) : (
-					<span key={index} className="font-bold">
-						{segment}
+					<span key={index}>
+						<Link href={segment.link}>{segment.display}</Link>
+						<span> / </span>
 					</span>
+				) : (
+					<Link href={segment.link} key={index} className="font-bold">
+						{segment.display}
+					</Link>
 				),
 			)}
 		</div>
