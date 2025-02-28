36d588b0abd644efaa4a37acb6241c2eb145393f
Ryan D <ryandodd0115@gmail.com>
2025-02-28 07:26:57 -0600

    Added an assignment for data

--- src/app/components/Dashboard/Announcements.tsx
+++ src/app/components/Dashboard/Announcements.tsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 import supabase from "../../utils/supabase";
 
 function Announcement({
+	key,
 	author,
 	date,
 	title,
 	content,
 }: {
+	key: number;
 	author: string;
 	date: string;
 	title: string;
 	content: string;
 }) {
 	return (
-		<div className="flex flex-col gap-1 rounded-sm p-[0.25rem]">
+		<div className="flex flex-col gap-1 rounded-sm p-[0.25rem]" key={key}>
 			<p className="text-2xl font-bold">{title}</p>
 			<span className="flex flex-row items-center justify-between pb-2">
 				<div className="flex items-center gap-2">
@@ -37,20 +39,19 @@ function formatDate(dateString: string): string {
 }
 
 export default async function Announcements() {
-	const { data } = await supabase
+	const { data: announcements } = await supabase
 		.from("announcements")
-		.select("creator_id,title,description,created_at")
+		.select("id,creator_id,title,description,created_at")
 		.order("created_at", { ascending: false })
 		.limit(5);
 
-	console.log(data);
-
 	return (
 		<div className="flex flex-col gap-[1.25rem]">
-			{data !== null &&
-				data.slice(0, 3).map((item, idx) => (
+			{announcements !== null &&
+				announcements.slice(0, 3).map((item, idx) => (
 					<>
 						<Announcement
+							key={item.id}
 							author={item.creator_id.toString()}
 							date={formatDate(item.created_at)}
 							title={item.title}
@@ -60,13 +61,13 @@ export default async function Announcements() {
 					</>
 				))}
 
-			{data !== null && data.length > 3 && (
+			{announcements !== null && announcements.length > 3 && (
 				<>
 					<div className="h-[1px] w-full bg-zinc-200" />
 					<Link
 						href="/announcements"
 						className="text-sm text-gray-500 hover:text-magenta hover:underline">
-						+ {data.length - 3} more
+						+ {announcements.length - 3} more
 					</Link>
 				</>
 			)}
