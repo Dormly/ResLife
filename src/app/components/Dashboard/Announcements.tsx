89717fe3bfe8013789f092d0aa1ae7b82edb4a15
Duncan Carr <dacarr@mail.bradley.edu>
2025-02-27 18:53:41 -0600

    Change styling on announcements

--- src/app/components/Dashboard/Announcements.tsx
+++ src/app/components/Dashboard/Announcements.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import exdb from "../exdb.json";
 
 const announcements = exdb.announcements;
@@ -25,14 +26,16 @@ function Announcement({
 	content: string;
 }) {
 	return (
-		<div className="flex flex-col rounded-sm p-[0.25rem]">
+		<div className="flex flex-col gap-1 rounded-sm p-[0.25rem]">
 			<p className="text-2xl font-bold">{title}</p>
-			<span className="flex flex-row items-center gap-[0.25rem]">
-				<div className="h-5 w-5 rounded-full bg-gray-400" />
-				<p className="">{author} |</p>
+			<span className="flex flex-row items-center justify-between pb-2">
+				<div className="flex items-center gap-2">
+					<div className="h-5 w-5 rounded-full bg-gray-400" />
+					<p className="font-medium">{author}</p>
+				</div>
 				<p>{date}</p>
 			</span>
-			<p>{content}</p>
+			<p className="opacity-60">{content}</p>
 		</div>
 	);
 }
@@ -40,14 +43,28 @@ function Announcement({
 export default function Announcements() {
 	return (
 		<div className="flex flex-col gap-[1.25rem]">
-			{announcements.map((item) => (
-				<Announcement
-					author={item.author}
-					date={item.date}
-					title={item.title}
-					content={item.content}
-				/>
+			{announcements.slice(0, 3).map((item, idx) => (
+				<>
+					<Announcement
+						author={item.author}
+						date={item.date}
+						title={item.title}
+						content={item.content}
+					/>
+					{idx < 2 && <div className="h-[1px] w-full bg-zinc-200" />}
+				</>
 			))}
+
+			{announcements.length > 3 && (
+				<>
+					<div className="h-[1px] w-full bg-zinc-200" />
+					<Link
+						href="/announcements"
+						className="text-sm text-gray-500 hover:text-magenta hover:underline">
+						+ {announcements.length - 3} more
+					</Link>
+				</>
+			)}
 		</div>
 	);
 }
