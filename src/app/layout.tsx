0b9bc9423230e6127640149ae8bd08f92950ec72
Ryan D <ryandodd0115@gmail.com>
2025-03-05 23:55:33 -0600

    Moved auth items to the callback features in route.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@ import "./globals.css";
 
 import { inter } from "@/app/ui/fonts";
 import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
 import SessionProvider from "./components/SessionProvider";
 import Sidebar from "@/app/components/Sidebar";
 import Topbar from "@/app/components/Topbar";
 import Reports from "./components/Reports";
-import supabase from "./utils/supabase";
 
 export const metadata: Metadata = {
 	title: "Dormly | ResLife",
@@ -22,54 +22,34 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }>) {
 	
-	const session = await getServerSession();
+	const session = await getServerSession(authOptions);
 
 	if (!session || !session.user) {
 		redirect("/api/auth/signin");
 	}
 
-	let { data: user } = await supabase
-		.from("users")
-		.select("id,email,name,profile,university_id(id, name)")
-		.eq("email", session.user.email == null ? "" : session.user.email)
-		.single();
-
-	if (user === null) {
-		console.log("User not found in DB, creating new user");
-		({ data: user } = await supabase
-			.from("users")
-			.insert([
-				{
-					email: session.user.email || "",
-					name: session.user.name || "",
-					profile: session.user.image || "",
-					university_id: 1,
-				},
-			])
-			.select("id,email,name,profile,university_id(id, name)")
-			.single());
-	}
+	const user = session.data;
 
 	return (
 		<html lang="en">
 			<SessionProvider session={session}>
-				<body className={`${inter.variable} overflow-x-clip antialiased`}>
-					<div className="flex h-svh w-svw flex-col">
-						<Topbar university={user !== null && user.university_id.name !== null ? user.university_id.name : ""}/>
-						<div className="flex h-full w-full flex-row overflow-hidden">
-							<div className="h-full w-[26rem] flex-shrink-0 overflow-y-auto">
-								<Sidebar />
-							</div>
-							<div className="flex w-full flex-1 flex-col overflow-y-auto">
-								{children}
+					<body className={`${inter.variable} overflow-x-clip antialiased`}>
+						<div className="flex h-svh w-svw flex-col">
+							<Topbar university={user !== null && user.university_id.name !== null ? user.university_id.name : ""}/>
+							<div className="flex h-full w-full flex-row overflow-hidden">
+								<div className="h-full w-[26rem] flex-shrink-0 overflow-y-auto">
+									<Sidebar />
+								</div>
+								<div className="flex w-full flex-1 flex-col overflow-y-auto">
+									{children}
+								</div>
 							</div>
 						</div>
-					</div>
 
-					<div className="fixed bottom-0 right-0 z-10 p-[1.25rem]">
-						<Reports />
-					</div>
-				</body>
+						<div className="fixed bottom-0 right-0 z-10 p-[1.25rem]">
+							<Reports />
+						</div>
+					</body>
 			</SessionProvider>
 		</html>
 	);
