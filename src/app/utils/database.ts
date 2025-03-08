24f906c1cacf1282512fea28c514a137bfb01a16
Ryan D <ryandodd0115@gmail.com>
2025-03-08 15:12:47 -0600

    Add new auth scheme for supabase

---
+++ src/app/utils/database.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import assert from "assert";
+import createServerClient from "./supabase/server";
+import { redirect } from "next/navigation";
+
+async function signInWith(provider: string) {
+	assert(provider === "google", "Only Google is supported");
+	assert(process.env.SUPABASE_URL, "env.SUPABASE_URL is not set");
+	assert(
+		process.env.SUPABASE_SERVICE_KEY,
+		"env.SUPABASE_SERVICE_KEY is not set",
+	);
+	assert(process.env.SITE_URL, "env.SITE_URL is not set");
+
+	const supabase = await createServerClient();
+	const authCallbackURL = `${process.env.SITE_URL}/api/auth/callback`;
+
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider: provider,
+		options: {
+			redirectTo: authCallbackURL,
+		},
+	});
+
+	if (error) {
+		console.error("Error signing in with Google:", error.message);
+		return {
+			status: 500,
+			body: "Internal Server Error",
+		};
+	}
+
+	redirect(data.url);
+}
+
+export async function signInWithGoogle() {
+	return await signInWith("google");
+}
