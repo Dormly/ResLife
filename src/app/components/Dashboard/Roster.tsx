d115f4bc299d99eceb382d4ad9c69e51a5696870
Ryan D <ryandodd0115@gmail.com>
2025-03-08 16:08:33 -0600

    Fix ResLife systems to access new table data based on uuid

--- src/app/components/Dashboard/Roster.tsx
+++ src/app/components/Dashboard/Roster.tsx
@@ -1,8 +1,5 @@
-import { getServerSession } from "next-auth";
-
+import createServerClient from "@/app/utils/supabase/server";
 import Link from "next/link";
-
-import supabase from "../../utils/supabase";
 
 interface RosterEntry {
 	fName: string;
@@ -33,52 +30,52 @@ function RosterEntry({ fName, lName, id, roomNo }: RosterEntry) {
 }
 
 async function RosterTable() {
-	// const session = await getServerSession(authOptions);
-	// if (!session) {
-	// 	// Handle the case when session is null
-	// 	console.error("Session is null");
-	// 	return null;
-	// }
-	// const user = session.data;
+	const supabase = await createServerClient();
+	const session = await supabase.auth.getUser();
 
-	// const { data: floor_staff } = await supabase
-	// 	.from("floor_staff")
-	// 	.select("user_id,floor_id(id,floor_name, building_id(abbreviation))")
-	// 	.eq("user_id", user.id);
+	if (!session.data.user) {
+		console.error("Session is null");
+		return null;
+	}
+
+	const { data: floor_staff } = await supabase
+		.from("floor_staff")
+		.select("user_uuid,floor_id(id,floor_name, building_id(abbreviation))")
+		.eq("user_uuid", session.data.user.id);
 
 	const roster: RosterEntry[] = [];
 
-	// if (floor_staff) {
-	// 	const promises = floor_staff.map(async (staff) => {
-	// 		const { data: student_bookings } = await supabase
-	// 			.from("student_bookings")
-	// 			.select(
-	// 				"student_id(id, first_name, last_name), room_space_id(room_id(room, floor_id(id,building_id(abbreviation))))",
-	// 			);
-	// 		if (student_bookings) {
-	// 			student_bookings.forEach((booking) => {
-	// 				if (booking.room_space_id.room_id.floor_id.id === staff.floor_id.id) {
-	// 					roster.push({
-	// 						fName: booking.student_id?.first_name
-	// 							? booking.student_id.first_name
-	// 							: "",
-	// 						lName: booking.student_id?.last_name
-	// 							? booking.student_id.last_name
-	// 							: "",
-	// 						id: booking.student_id?.id
-	// 							? booking.student_id.id.toString()
-	// 							: "",
-	// 						roomNo:
-	// 							staff.floor_id.building_id.abbreviation +
-	// 							" " +
-	// 							booking.room_space_id.room_id.room,
-	// 					});
-	// 				}
-	// 			});
-	// 		}
-	// 	});
-	// 	await Promise.all(promises);
-	// }
+	if (floor_staff) {
+		const promises = floor_staff.map(async (staff) => {
+			const { data: student_bookings } = await supabase
+				.from("student_bookings")
+				.select(
+					"student_id(id, first_name, last_name), room_space_id(room_id(room, floor_id(id,building_id(abbreviation))))",
+				);
+			if (student_bookings) {
+				student_bookings.forEach((booking) => {
+					if (booking.room_space_id.room_id.floor_id.id === staff.floor_id.id) {
+						roster.push({
+							fName: booking.student_id?.first_name
+								? booking.student_id.first_name
+								: "",
+							lName: booking.student_id?.last_name
+								? booking.student_id.last_name
+								: "",
+							id: booking.student_id?.id
+								? booking.student_id.id.toString()
+								: "",
+							roomNo:
+								staff.floor_id.building_id.abbreviation +
+								" " +
+								booking.room_space_id.room_id.room,
+						});
+					}
+				});
+			}
+		});
+		await Promise.all(promises);
+	}
 
 	//.lte('start_date', Date.now());
 	// .lte('end_date', Date.now());
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
 						href="/announcements"
 						Icon={Megaphone}
 						title="Announcements">
-						{/* <Announcements /> */}
+						<Announcements />
 					</SidebarItem>
 				</div>
 			</div>
